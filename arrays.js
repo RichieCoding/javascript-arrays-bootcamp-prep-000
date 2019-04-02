65dd0fa2f53aa9bc528d2a06e16368d43130156a
Richard Li <richardli125@gmail.com>
2019-04-02 05:40:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,21 +1,17 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
-
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
 
-
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
-
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
@@ -40,3 +36,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
 }
 
+function removeElementFromEndOfArray() {
+  
+}
