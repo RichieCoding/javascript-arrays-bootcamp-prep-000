76bcd20e65eeb134728dfbbed991e116972be56a
Richard Li <richardli125@gmail.com>
2019-04-02 05:07:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift(1);
+  array.shift(1);
+  return array;
 }
 
