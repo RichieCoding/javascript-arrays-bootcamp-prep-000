0ec318f5d1a5b89462ffd7e036281c79d161fc3b
Richard Li <richardli125@gmail.com>
2019-04-02 04:24:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,9 +4,11 @@ function addElementToBeginningOfArray(array, element) {
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array;
 }
 function addElementToEndOfArray(array, element) {
   array.push(element);
+  return array;
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   
